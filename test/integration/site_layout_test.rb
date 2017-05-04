6012b9d1c4cc44e83e7ad1021074cabe814d795e
Viktor Schmidt <vschmidt@ckc.de>
2017-05-04 17:39:23 +0200

    Add users controller and sign up page Finish layout and routes

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -21,5 +21,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "title", full_title("About")
     get contact_path
     assert_select "title", full_title("Contact")
+    get signup_path
+    assert_select "title", full_title("Sign up")
   end
 end
