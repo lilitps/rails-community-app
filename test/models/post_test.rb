62b395a03ae098113b7b3ec0d1d379fb8a05dfdf
Viktor Schmidt <vschmidt@ckc.de>
2017-05-24 13:54:11 +0200

    Add user posts

---
+++ test/models/post_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class PostTest < ActiveSupport::TestCase
+  def setup
+    @user = users(:michael)
+    @post = @user.posts.build(content: "Lorem ipsum")
+  end
+
+  test "should be valid" do
+    assert @post.valid?
+  end
+
+  test "user id should be present" do
+    @post.user_id = nil
+    assert_not @post.valid?
+  end
+
+  test "content should be present" do
+    @post.content = "   "
+    assert_not @post.valid?
+  end
+
+  test "content should be at most 5000 characters" do
+    @post.content = "a" * 5001
+    assert_not @post.valid?
+  end
+end
