234b85e345ff2d173b82b883d0c29677ea8a8fc3
Viktor Schmidt <vschmidt@ckc.de>
2017-06-16 12:15:39 +0200

    Add subselects to feed implementation and includes user

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,10 +3,14 @@ module PostsHelper
 
   # Returns 5 posts per page
   def feed(page, per_page = 5, only_admin = true)
-    query = Post.joins(:user).where(users: {admin: only_admin})
-    query = query.or(Post.joins(:user)
-                         .where(users: {admin: !only_admin})
-                         .where("user_id IN (?) OR user_id = ?", current_user.following_ids, current_user.id)) if logged_in?
+    query = Post.includes(:user).where(users: {admin: only_admin})
+    if logged_in?
+      following_ids = "SELECT followed_id FROM relationships WHERE  follower_id = :user_id"
+      query = query.or(Post.includes(:user)
+                           .where(users: {admin: !only_admin})
+                           .where("user_id IN (#{following_ids}) OR user_id = :user_id",
+                                  user_id: current_user.id))
+    end
     query.paginate(page: page, per_page: per_page)
   end
 end
