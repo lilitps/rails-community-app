77fc2220a13764d61df6ad1ccdb4b273672b1e5a
Viktor Schmidt <vschmidt@ckc.de>
2017-07-05 16:53:18 +0200

    Fix config for locale load path and listen library

--- config/initializers/locales.rb
+++ config/initializers/locales.rb
@@ -1,5 +1,5 @@
 # Where the I18n library should search for translation files
-I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
+I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
 
 # Whitelist locales available for the application
 # Adds locales available for the application to scope in routes.rb
