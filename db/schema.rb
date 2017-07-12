4fc0b1e16d3ebe55f9c5c86cc45f7bd19045704a
Viktor Schmidt <vschmidt@ckc.de>
2017-07-12 16:57:40 +0200

    Fix users locale column migration

--- db/schema.rb
+++ db/schema.rb
@@ -45,7 +45,7 @@ ActiveRecord::Schema.define(version: 20170623101332) do
     t.datetime "activated_at"
     t.string "reset_digest"
     t.datetime "reset_sent_at"
-    t.string "locale"
+    t.string "locale", default: "en"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
