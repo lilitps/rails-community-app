126f7012c469a099a8a8e6a41bb8087492e73690
Viktor Schmidt <vschmidt@ckc.de>
2017-08-02 12:25:32 +0200

    Refactor viewport and navbar to mobile friendly view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,13 @@ module ApplicationHelper
       page_title + " | " + base_title
     end
   end
+
+  # Returns viewport meta tag to ensure proper rendering and touch zooming
+  def mobile_first_viewport_meta_tag
+    if protect_against_forgery?
+      [
+          tag("meta", name: "viewport", content: 'width=device-width, initial-scale=1')
+      ].join("\n").html_safe
+    end
+  end
 end
