16118969833882e131753578e4d4d0cef9a7b81e
Viktor Schmidt <vschmidt@ckc.de>
2017-08-04 12:43:36 +0200

    Add tests for popups end edit forms

--- test/integration/site_feed_test.rb
+++ test/integration/site_feed_test.rb
@@ -9,7 +9,7 @@ class SiteFeedTest < ActionDispatch::IntegrationTest
     @non_admin = users(:archer)
   end
 
-  test "should display first 5 posts on home page feed" do
+  test "should display first 3 posts on home page feed" do
     get root_path
     assert_template 'static_pages/home'
     assert_select 'div.pagination-sm', count: 1
@@ -69,6 +69,52 @@ class SiteFeedTest < ActionDispatch::IntegrationTest
       assert @feed.include?(post)
       assert_match CGI.escapeHTML(post.content), response.body
     end
+  end
+
+  test "should render all image and edit modals for all posts" do
+    # without log in
+    get root_path
+    assert_template 'static_pages/home'
+    @feed = assigns(:feed)
+    assert_not @feed.empty?
+    assert_select '#all-modals', count: 1
+    @feed.each do |post|
+      assert_select '#image-modal-' + post.id.to_s, count: 1 if post.picture?
+      assert_select '#imagePost' + post.id.to_s + 'ModalLabel', count: 1 if post.picture?
+      assert_select '#imagePost' + post.id.to_s + 'ModalLabel', post.picture.file.basename if post.picture?
+      assert_match post.picture.file.basename, response.body if post.picture?
+      assert_select '#edit-post-modal-' + post.id.to_s, false, "home page must contain no modals"
+    end
+    # log in as non admin user
+    log_in_as(@non_admin)
+    get root_path
+    assert_template 'static_pages/home'
+    @feed = assigns(:feed)
+    assert_not @feed.empty?
+    assert_select '#all-modals', count: 1
+    @feed.each do |post|
+      assert_select '#image-modal-' + post.id.to_s, count: 1 if post.picture?
+      assert_select '#edit-post-modal-' + post.id.to_s, false, "home page must contain no modals"
+    end
+    assert_select '#new-post-modal', false, "home page must contain no new post modal"
+    # log in as admin user
+    log_in_as(@admin)
+    get root_path
+    assert_template 'static_pages/home'
+    @feed = assigns(:feed)
+    assert_not @feed.empty?
+    assert_select '#all-modals', count: 1
+    @feed.each do |post|
+      assert_select '#image-modal-' + post.id.to_s, count: 1 if post.picture?
+      assert_select '#edit-post-modal-' + post.id.to_s, true, "home page must contain edit modals"
+      assert_select '#editPost' + post.id.to_s + 'ModalLabel', count: 1
+      assert_select '#editPost' + post.id.to_s + 'ModalLabel', 'Edit post...'
+      assert_select '#edit_post_' + post.id.to_s, count: 1
+    end
+    assert_select '#new-post-modal', true, "home page must contain new post modal"
+    assert_select '#editPostModalLabel', count: 1
+    assert_select '#editPostModalLabel', 'Compose new post...'
+    assert_select '#new_post', count: 1
   end
 
   test "should display first 3 facebook page posts on home page feed" do
