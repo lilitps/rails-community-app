63fdbd83db8c7dfaf01087d25932e66e70397897
Viktor Schmidt <vschmidt@ckc.de>
2017-09-08 14:46:58 +0200

    Fix meta tag offenses by RuboCop

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Adds helper methods to be used in context of this application
 module ApplicationHelper
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
@@ -6,15 +9,6 @@ module ApplicationHelper
       base_title
     else
       page_title + ' | ' + base_title
-    end
-  end
-
-  # Returns viewport meta tag to ensure proper rendering and touch zooming
-  def mobile_first_viewport_meta_tag
-    if protect_against_forgery?
-      [
-        tag('meta', name: 'viewport', content: 'width=device-width, initial-scale=1')
-      ].join("\n").html_safe
     end
   end
 end
