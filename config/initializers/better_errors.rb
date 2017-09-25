a1637ef1d2d18a5a2c089ba61b40298739901cce
Viktor Schmidt <vschmidt@ckc.de>
2017-09-25 16:12:26 +0200

    Fix BetterErrors exception handling in development env

--- config/initializers/better_errors.rb
+++ config/initializers/better_errors.rb
@@ -1,11 +1,12 @@
-# e.g. in config/initializers/better_errors.rb
+# frozen_string_literal: true
+
 # This will stop BetterErrors from trying to render larger objects, which can cause
 # slow loading times and browser performance problems. Stated size is in characters and refers
 # to the length of #inspect's payload for the given object. Please be aware that HTML escaping
 # modifies the size of this payload so setting this limit too precisely is not recommended.
-# default value: 100_000
-# credits to GitHub user bquorning, MIT License 2017: https://makandracards.com/makandra/46175-speed-up-better_errors
 if defined?(BetterErrors) && Rails.env.development?
+  # default value: 100_000
+  # credits to GitHub user bquorning, MIT License 2017: https://makandracards.com/makandra/46175-speed-up-better_errors
   module BetterErrorsHugeInspectWarning
     def inspect_value(obj)
       inspected = obj.inspect
@@ -15,11 +16,11 @@ if defined?(BetterErrors) && Rails.env.development?
       CGI.escapeHTML(inspected)
     rescue NoMethodError
       "<span class='unsupported'>(object doesn't support inspect)</span>"
-    rescue Exception
+    rescue StandardError
       "<span class='unsupported'>(exception was raised in inspect)</span>"
     end
   end
 
-  BetterErrors.ignored_instance_variables += [ :@_request, :@_assigns, :@_controller, :@view_renderer ]
+  BetterErrors.ignored_instance_variables += %i[@_request @_assigns @_controller @view_renderer]
   BetterErrors::ErrorPage.prepend(BetterErrorsHugeInspectWarning)
 end
