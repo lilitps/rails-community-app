30276e2e3a6b77346826e528baf07464b53481f0
Viktor Schmidt <vschmidt@ckc.de>
2017-09-29 15:47:18 +0200

    Add more sensitive parameters which will be filtered from the log file

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -3,4 +3,8 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += %i[password password_confirmation]
+Rails.application.config.filter_parameters += %i[ password
+                                                  password_confirmation
+                                                  appsecret_proof
+                                                  client_secret
+                                                  access_token ]
