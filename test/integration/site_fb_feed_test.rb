5b28dafa0eb187e8530b9da40a71eda6be76ec91
Viktor Schmidt <vschmidt@ckc.de>
2017-10-05 11:10:42 +0200

    Fix facebook feed thumbnails stamp and test description

--- test/integration/site_fb_feed_test.rb
+++ test/integration/site_fb_feed_test.rb
@@ -16,15 +16,24 @@ class SiteFbFeedTest < ActionDispatch::IntegrationTest
       feed = fb_feed
       assert_not feed.empty?
       feed.each do |post|
+        # post message
         assert_select 'div#post-' + post['id'], count: 1
+        assert_select 'a[href=?]', post['permalink_url']
         if post['message'].present? && post['message'].length > 150
           assert_match auto_format_html(truncate(post['message'], length: 150)), response.body
           assert_select '.read-more-' + post['id'], count: 1
-          assert_select '.read-more-description-' + post['id'], count: 1
         else
           assert_match auto_format_html(post['message']), response.body
         end
-        assert_select 'a[href=?]', post['permalink_url']
+        # link and description
+        assert_select 'a', post['name'] ? post['name'] : '', post['link']
+        assert_select 'a[href=?]', post['link']
+        if post['description'].present? && post['description'].length > 150
+          assert_match auto_format_html(truncate(post['description'], length: 150)), response.body
+          assert_select '.read-more-description-' + post['id'], count: 1
+        else
+          assert_match auto_format_html(post['description']), response.body
+        end
       end
     end
   end
