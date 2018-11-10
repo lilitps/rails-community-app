a40e1263b0390ec10addbc787429690cf3a85365
Viktor Schmidt <vschmidt@anynines.com>
2018-11-10 20:07:18 +0100

    Change contact views to slim

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -2,25 +2,15 @@
 
 # Adds helper methods to be used in context of a contact
 module ContactsHelper
-  # Integrate the Google Maps API v3
-  # https://developers.google.com/maps/documentation/javascript/adding-a-google-map
-  # see further google_maps.coffee
-
-  # take care to use the init google maps coffee script before the google maps script tag
-  # returns the src string to get access to the dynamic (Java Script) map API.
-  def google_dynamic_js_map
-    "#{google_maps_api_url}/api/js?key=#{google_maps_api_key}&language=#{I18n.locale}&callback=initGoogleMap"
+  def about_chairman
+    "#{t('community.about_board_of_directors_chairman')} : #{t('community.about_board_of_directors_chairman_name')}"
   end
 
-  private
-
-  # returns the google maps api key
-  def google_maps_api_key
-    ENV['GOOGLE_MAPS_API_KEY']
+  def about_vice_chairman
+    "#{t('community.about_board_of_directors_vice_chairman')} : #{t('community.about_board_of_directors_vice_chairman_name')}"
   end
 
-  # returns the API url from the Google Maps API v3
-  def google_maps_api_url
-    'https://maps.googleapis.com/maps'
+  def about_treasurer
+    "#{t('community.about_board_of_directors_treasurer')} : #{t('community.about_board_of_directors_treasurer_name')}"
   end
 end
