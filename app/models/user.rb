4569ff2e4d573e7b4cb78075a261bb3f6d74652e
Viktor Schmidt <viktorianer4life@gmail.com>
2019-05-28 16:33:28 +0200

    Clean up

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,6 +37,8 @@
 
 # An application user
 class User < ApplicationRecord
+  before_save :downcase_email
+
   has_many :posts, dependent: :destroy
   has_many :active_relationships, class_name: 'Relationship',
                                   foreign_key: 'follower_id',
@@ -48,7 +50,6 @@ class User < ApplicationRecord
                                    inverse_of: 'followed'
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
-  before_save :downcase_email
 
   validates :name, presence: true, length: { maximum: 50 }
 
